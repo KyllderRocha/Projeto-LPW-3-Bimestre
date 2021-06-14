73898c7b7e9446cea6688305798ad0f723683b6d
Kyllder Rocha <kyllder.rocha@gmail.com>
2021-06-13 23:54:21 -0300

    Subindo Projeto

---
+++ LPW/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LPW.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ LPW/Models/FormModel.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace LPW.Models
+{
+    public class FormModel
+    {
+        public string nome { get; set; }
+        public string email { get; set; }
+        public string telefone { get; set; }
+        public string caso { get; set; }
+
+    }
+}
